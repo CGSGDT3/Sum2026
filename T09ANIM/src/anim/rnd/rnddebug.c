f21b252d26e80af02c8c44f0fe8941014982424e
CGSGDT3 <dukeofcaladanplanet@gmail.com>
2026-06-14 23:22:41 +0300

    Update rnddebug.c

--- T09ANIM/src/anim/rnd/rnddebug.c
+++ T09ANIM/src/anim/rnd/rnddebug.c
@@ -37,23 +37,11 @@ VOID APIENTRY glDebugOutput( UINT Source, UINT Type, UINT Id, UINT Severity,
   CONSOLE_FONT_INFOEX cfi = {0};
   HWND hConWnd;
 
-  /* Create console */
-  AllocConsole();
-
   cfi.cbSize = sizeof(CONSOLE_FONT_INFOEX);
   GetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
   cfi.dwFontSize.Y = 18;
   cfi.FontWeight = FW_BOLD;
   SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
-
-  freopen("CONOUT$", "w", stdout);
-  system("@chcp 1251 > nul");
-  printf("\x1b[38;2;%d;%d;%dm \x1b[48;2;%d;%d;%dm", 255, 255, 0, 0, 102, 102);
-  printf("Группа компьютерной графики ФМЛ № 30\n");
-  printf("\x1b[38;2;%d;%d;%dm\x1b[48;2;%d;%d;%dm", 0, 255, 0, 90, 90, 90);
-  printf("Computer Graphics Support Group\n");
-  printf("\x1b[38;2;%d;%d;%dm\x1b[48;2;%d;%d;%dm", 255, 255, 255, 0, 0, 0);
-  fflush(stdout);
 
   hConWnd = GetConsoleWindow();
   SetWindowPos(hConWnd, HWND_TOP, 0, 0, 470, 470, 0);
